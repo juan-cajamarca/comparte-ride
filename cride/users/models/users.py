2128b2153f8e90c80f96ba71543e6f2838680c36
Juan Cajamarca <juancajamarca@outlook.es>
2020-06-25 11:15:08 -0500

    Ordering models in a Django package

--- cride/users/models/users.py
+++ cride/users/models/users.py
@@ -1,6 +1,7 @@
 # Django
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Utilities
 from cride.utils.models import CRideModel
@@ -15,12 +16,20 @@ class User(CRideModel, AbstractUser):
     email = models.EmailField(
         'email address',
         unique=True,
-        error_message={
+        error_messages={
             'unique': 'An user with that email already exists.'
         }
     )
 
-    phone_number = models.CharField(max_length=17, blank=True)
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message='Phone number must be entered in the format: +999999999.'
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex],
+        max_length=17,
+        blank=True
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
@@ -39,3 +48,9 @@ class User(CRideModel, AbstractUser):
         default=False,
         help_text='Set to true when the user have verified the email address.'
     )
+
+    def __str__(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username

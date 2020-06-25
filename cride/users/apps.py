dc0a8535b4f019074b7b3c123f3a565b96a02bf5
Juan Cajamarca <juancajamarca@outlook.es>
2020-06-25 10:49:52 -0500

    Users app created

---
+++ cride/users/apps.py
@@ -0,0 +1,6 @@
+# Django
+from django.apps import AppConfig
+
+class UsersAppConfig(AppConfig):
+    name = 'cride.users'
+    verbose_name = 'Users'
---
+++ cride/users/models.py
@@ -0,0 +1,41 @@
+# Django
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Utilities
+from cride.utils.models import CRideModel
+
+
+class User(CRideModel, AbstractUser):
+    """
+    Extend from Django's Abstract User, change the username field to email
+    and add some extra fields.
+    """
+
+    email = models.EmailField(
+        'email address',
+        unique=True,
+        error_message={
+            'unique': 'An user with that email already exists.'
+        }
+    )
+
+    phone_number = models.CharField(max_length=17, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    is_client = models.BooleanField(
+        'client status',
+        default=True,
+        help_text=(
+            'Help easily distinguish users and perform queries.'
+            'Clients are the main type of user.'
+        )
+    )
+
+    is_verified = models.BooleanField(
+        'verified',
+        default=False,
+        help_text='Set to true when the user have verified the email address.'
+    )

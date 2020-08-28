8daa4bbd6bc82dab4b64e715ec8612088bf96840
Juan Cajamarca <juancajamarca@outlook.es>
2020-08-28 12:40:25 -0500

    DRF Views, URLs and Parsers

---
+++ cride/circles/urls.py
@@ -0,0 +1,11 @@
+# Django
+from django.urls import path
+
+# Views
+from cride.circles.views import list_circles, create_circle
+
+
+urlpatterns = [
+    path('circles/', list_circles),
+    path('circles/create/', create_circle)
+]

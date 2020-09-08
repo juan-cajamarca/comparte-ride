33afe58a33025f202563e5acb5e69ab0c1b26b69
Juan Cajamarca <juancajamarca@outlook.es>
2020-09-07 22:21:01 -0500

    User Signup done

--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -4,7 +4,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 # Serializers
-from cride.users.serializers import UserLoginSerializer, UserModelSerializer
+from cride.users.serializers import (
+    UserLoginSerializer,
+    UserModelSerializer,
+    UserSignUpSerializer
+)
 
 
 class UserLoginAPIView(APIView):
@@ -16,4 +20,12 @@ class UserLoginAPIView(APIView):
             'user': UserModelSerializer(user).data,
             'access_token': token
         }
+        return Response(data, status=status.HTTP_201_CREATED)
+
+class UserSignUpAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = UserModelSerializer(user).data
         return Response(data, status=status.HTTP_201_CREATED)

011733dc930c6c4d9f8526b59a429e910acc6fed
Juan Cajamarca <juancajamarca@outlook.es>
2020-09-16 12:42:14 -0500

    Circles update - Custom permission & DRF Mixins

---
+++ cride/circles/permissions/circles.py
@@ -0,0 +1,19 @@
+# Django REST Framework
+from rest_framework.permissions import BasePermission
+
+# Models
+from cride.circles.models import Membership
+
+
+class IsCircleAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        try:
+            Membership.objects.get(
+                user=request.user,
+                circle=obj,
+                is_admin=True,
+                is_active=True
+            )
+        except Membership.DoesNotExist:
+            return False
+        return True

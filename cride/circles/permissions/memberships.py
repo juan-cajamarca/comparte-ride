02252fb13e8720af9c7b79991a1a05e38a744d67
Juan Cajamarca <juancajamarca@outlook.es>
2020-09-22 11:33:04 -0500

    Retrieve and Destroy member

---
+++ cride/circles/permissions/memberships.py
@@ -0,0 +1,18 @@
+# Django REST Framework
+from rest_framework.permissions import BasePermission
+
+# Models
+from cride.circles.models import Membership
+
+
+class IsActiveCircleMember(BasePermission):
+    def has_permission(self, request, view):
+        try:
+            Membership.objects.get(
+                user=request.user,
+                circle=view.circle,
+                is_active=True
+            )
+        except Membership.DoesNotExist:
+            return False
+        return True
--- cride/circles/views/memberships.py
+++ cride/circles/views/memberships.py
@@ -5,11 +5,15 @@ from rest_framework.generics import get_object_or_404
 # Serializers
 from cride.circles.serializers import MembershipModelSerializer
 
+# Permissions
+from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.memberships import IsActiveCircleMember
+
 # Models
 from cride.circles.models import Circle, Membership
 
 
-class MembershipViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class MembershipViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
     serializer_class = MembershipModelSerializer
 
     def dispatch(self, request, *args, **kwargs):
@@ -18,5 +22,22 @@ class MembershipViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
         self.circle = get_object_or_404(Circle, slug_name=slug_name)
         return super(MembershipViewSet, self).dispatch(request, *args, **kwargs)
 
+    def get_permissions(self):
+        """Assign permissions based on action"""
+        permissions = [IsAuthenticated, IsActiveCircleMember]
+        return [p() for p in permissions]
+
     def get_queryset(self):
         return Membership.objects.filter(circle=self.circle, is_active=True)
+
+    def get_object(self):
+        return get_object_or_404(
+            Membership,
+            user__username=self.kwargs['pk'],
+            circle=self.circle,
+            is_active=True
+        )
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()

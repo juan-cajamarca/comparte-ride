bf5402471cc669bf9b291dba7214f770977edaa2
Juan Cajamarca <juancajamarca@outlook.es>
2020-09-22 11:12:19 -0500

    List members - Nested resources

---
+++ cride/circles/views/memberships.py
@@ -0,0 +1,22 @@
+# Django REST Framework
+from rest_framework import mixins, viewsets
+from rest_framework.generics import get_object_or_404
+
+# Serializers
+from cride.circles.serializers import MembershipModelSerializer
+
+# Models
+from cride.circles.models import Circle, Membership
+
+
+class MembershipViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = MembershipModelSerializer
+
+    def dispatch(self, request, *args, **kwargs):
+        """Verifying that the circle exists"""
+        slug_name = kwargs['slug_name']
+        self.circle = get_object_or_404(Circle, slug_name=slug_name)
+        return super(MembershipViewSet, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return Membership.objects.filter(circle=self.circle, is_active=True)
